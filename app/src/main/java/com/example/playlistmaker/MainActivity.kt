eee56d0cac0205dbad1e1ba12f3917594a5713b8
ovseychik <github@ovseychik.me>
2023-03-23 10:31:51 +0600

    Revert "Домашнее задание №1"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,32 +1,11 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonSearch = findViewById<Button>(R.id.btn_search)
-        val buttonMedia = findViewById<Button>(R.id.btn_media)
-        val buttonSettings = findViewById<Button>(R.id.btn_settings)
-
-        buttonSearch.setOnClickListener {
-            val searchIntent = Intent(this, SearchActivity::class.java)
-            startActivity(searchIntent)
-        }
-
-        buttonMedia.setOnClickListener {
-            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
-            startActivity(mediaLibraryIntent)
-        }
-
-        buttonSettings.setOnClickListener {
-            val settingsIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingsIntent)
-        }
     }
 }
